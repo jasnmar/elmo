672da943dcbb1ba34ca6d1d7143c84797fbb815f
jasnmar <jasnmar@gmail.com>
2024-12-05 12:23:42 -0500

    clean up

--- src/main.js
+++ src/main.js
@@ -51,17 +51,18 @@ document.querySelector('#app').innerHTML = `
     </div>
     <div class="control">
       <button id="elmo" class="character-btn">Elmo</button>
-      <button id="cookie" class="character-btn">Cookie</button>
+      <button id="cookie" class="character-btn cookie-color">Cookie</button>
     </div>
   </div>
 `
 
+//State management
 let eyePosLeft = 0
 let eyePosTop = 0
 let mouthOpen = 88
 
 
-
+//UI Elements
 const partyButton = document.getElementById("party")
 const eyelrSlider = document.getElementById("eye-lr")
 const eyeudSlider = document.getElementById("eye-ud")
@@ -69,102 +70,40 @@ const mouthSlider = document.getElementById("mouth-input")
 const cookieBtn = document.getElementById('cookie')
 const elmoBtn = document.getElementById('elmo')
 
-makeElmo()
 
+//Event Listeners
 
-function makeCookie() {
-  const head = document.getElementById('head')
-  head.classList = ''
-  head.classList.add('cookie')
-  const nose = document.getElementById('nose')
-  nose.classList = ''
-  nose.classList.add('cookie--nose')
-  const mouth = document.getElementById('mouth')
-  mouth.classList = ''
-  mouth.classList.add('cookie--mouth')
-  const eyes = document.getElementsByClassName('eye')
-  for(let eye of eyes) {
-    eye.classList = ''
-    eye.classList.add('eye')
-    eye.classList.add('cookie--eye')
-  }
-}
-
-function makeElmo() {
-  const head = document.getElementById('head')
-  head.classList = ''
-  head.classList.add('elmo')
-  const nose = document.getElementById('nose')
-  nose.classList = ''
-  nose.classList.add('elmo--nose')
-  const mouth = document.getElementById('mouth')
-  mouth.classList = ''
-  mouth.classList.add('elmo--mouth')
-  const eyes = document.getElementsByClassName('eye')
-  for(let eye of eyes) {
-    eye.classList = ''
-    eye.classList.add('eye')
-    eye.classList.add('elmo--eye')
-  }
-}
-
-function updateEyeLeft(left) {
-  eyePosLeft = left
-  eyelrSlider.value = left+50
-  const eyes = document.getElementsByClassName('eyeball')
-  if(eyes) {
-    for(let eye of eyes) {
-      eye.style.left = eyePosLeft.toString() + "px"
-    }
-  }
-}
-
-function updateEyeTop(top) {
-  eyePosTop = top
-  eyeudSlider.value = top
-  const eyes = document.getElementsByClassName('eyeball')
-  if(eyes) {
-    for(let eye of eyes) {
-      eye.style.top = eyePosTop.toString() + "px"
-    }
-  }
-}
-
-function updateMouth(opening) {
-  mouthOpen = opening
-  mouthSlider.value = opening
-  const mouth = document.getElementById('mouth')
-  if(mouth) {
-    mouth.style.transform = `rotateX(${opening}deg)`
-  }
-}
-
-cookieBtn.addEventListener('click', (e) => {
-  e.preventDefault()
-  makeCookie()
-})
-
+//Elmo Button
 elmoBtn.addEventListener('click', (e) => {
   e.preventDefault()
   makeElmo()
 })
 
+//Cookie Monster Button
+cookieBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+  makeCookie()
+})
 
+//Eye Left - Right Slider
 eyelrSlider.addEventListener("input", (e) => {
   const lf = e.target.value - 50
   updateEyeLeft(lf) 
 })
 
+//Eye Up - Down Slider
 eyeudSlider.addEventListener("input", (e) => {
   const ud = e.target.value
   updateEyeTop(ud)
 })
 
+//Mouth Slider
 mouthSlider.addEventListener("input", (e) => {
   const rotate = e.target.value
   updateMouth(rotate)
 })
 
+//Confetti Button
 partyButton.addEventListener("click", () => {
   const jsConfetti = new JSConfetti()
   jsConfetti.addConfetti()
@@ -198,6 +137,88 @@ partyButton.addEventListener("click", () => {
   
 })
 
+//Start with Elmo
+makeElmo()
+
+
+function makeCookie() {
+  const head = document.getElementById('head')
+  head.classList = ''
+  head.classList.add('cookie')
+  const nose = document.getElementById('nose')
+  nose.classList = ''
+  nose.classList.add('cookie--nose')
+  const mouth = document.getElementById('mouth')
+  mouth.classList = ''
+  mouth.classList.add('cookie--mouth')
+  const eyes = document.getElementsByClassName('eye')
+  for(let eye of eyes) {
+    eye.classList = ''
+    eye.classList.add('eye')
+    eye.classList.add('cookie--eye')
+  }
+  const partyButton = document.getElementById('party')
+  partyButton.classList.add('cookie-color')
+  partyButton.classList.remove('elmo-color')
+}
+
+function makeElmo() {
+  const head = document.getElementById('head')
+  head.classList = ''
+  head.classList.add('elmo')
+  const nose = document.getElementById('nose')
+  nose.classList = ''
+  nose.classList.add('elmo--nose')
+  const mouth = document.getElementById('mouth')
+  mouth.classList = ''
+  mouth.classList.add('elmo--mouth')
+  const eyes = document.getElementsByClassName('eye')
+  for(let eye of eyes) {
+    eye.classList = ''
+    eye.classList.add('eye')
+    eye.classList.add('elmo--eye')
+  }
+  const partyButton = document.getElementById('party')
+  partyButton.classList.add('elmo-color')
+  partyButton.classList.remove('cookie-color')
+}
+
+
+//Utility functions that actually update the character features
+
+function updateEyeLeft(left) {
+  eyePosLeft = left
+  eyelrSlider.value = left+50
+  const eyes = document.getElementsByClassName('eyeball')
+  if(eyes) {
+    for(let eye of eyes) {
+      eye.style.left = eyePosLeft.toString() + "px"
+    }
+  }
+}
+
+function updateEyeTop(top) {
+  eyePosTop = top
+  eyeudSlider.value = top
+  const eyes = document.getElementsByClassName('eyeball')
+  if(eyes) {
+    for(let eye of eyes) {
+      eye.style.top = eyePosTop.toString() + "px"
+    }
+  }
+}
+
+function updateMouth(opening) {
+  mouthOpen = opening
+  mouthSlider.value = opening
+  const mouth = document.getElementById('mouth')
+  if(mouth) {
+    mouth.style.transform = `rotateX(${mouthOpen}deg)`
+  }
+}
+
+//If the person doesn't want to use the sliders they can click
+//on the canvas to update either the eyes or the mouth
 const clickDiv = document.getElementById("click")
 if(clickDiv) {
   clickDiv.addEventListener("click", (e) => {
@@ -224,7 +245,7 @@ if(clickDiv) {
 //This converter assumes we are looking for a normalized 
 //distance and angle from the center of the element clicked
 function clickConverter(event) {
-  const debug = false
+  const debug = true
 
   //What element was clicked?
   const parentElement = event.target
